070e36ed59b98209d9d3c98baa4d5c89c4f3709a
ninzzya <s.a.azarov@gmail.com>
2020-06-03 22:28:17 +0300

    Code review

--- js/main.js
+++ js/main.js
@@ -4,18 +4,9 @@
 
   var mainMap = new window.MainMap();
   var pins = new window.Pins();
-  var mainPin = new window.MainPin();
 
   var orders = window.generateOrders(ORDER_COUNT);
   var listPins = [];
-  var evtCoord = {
-    x: 0,
-    y: 0,
-    shiftX: 0,
-    shiftY: 0,
-    pinX: 0,
-    pinY: 0,
-  };
 
   /**
    * @description Отрисовывает список пинов на карте, если карта была неактивной
@@ -32,44 +23,7 @@
     });
   }
 
-  function onMainPinMouseMove(evt) {
-    evt.preventDefault();
-
-    evtCoord.shiftX = evtCoord.x - evt.clientX;
-    evtCoord.shiftY = evtCoord.y - evt.clientY;
-    evtCoord.x = evt.clientX;
-    evtCoord.y = evt.clientY;
-    evtCoord.pinX = window.Utils.setCoordX(mainPin.getElement().offsetLeft - evtCoord.shiftX);
-    evtCoord.pinY = window.Utils.setCoordY(mainPin.getElement().offsetTop - evtCoord.shiftY);
-
-    mainPin.getElement().style.left = evtCoord.pinX + 'px';
-    mainPin.getElement().style.top = evtCoord.pinY + 'px';
-  }
-
-  function onMainPinMouseUp(evt) {
-    evt.preventDefault();
-    document.removeEventListener('mousemove', onMainPinMouseMove);
-    document.removeEventListener('mouseup', onMainPinMouseUp);
-  }
-
-  function onMainPinMousedown() {
-    mainPin.getElement().addEventListener('mousedown', function (evt) {
-      evt.preventDefault();
-
-      if (!mainMap._isActivate) {
-        renderMainMap();
-      }
-
-      evtCoord.x = evt.clientX;
-      evtCoord.y = evt.clientY;
-
-      document.addEventListener('mousemove', onMainPinMouseMove);
-      document.addEventListener('mouseup', onMainPinMouseUp);
-    });
-  }
-
   mainMap.getElement();
   window.inf.mapInf = document.querySelector('.map').getBoundingClientRect();
-  mainPin.onMousedown = onMainPinMousedown;
-  mainPin.onMousedown();
+  renderMainMap();
 })();
