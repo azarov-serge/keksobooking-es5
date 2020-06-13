7faae99f1e38fa8c3f3304ba4d395ac28ea953ec
ninzzya <s.a.azarov@gmail.com>
2020-06-14 01:59:21 +0300

    Bug fix (Card event listeners)

--- js/controllers/pin.js
+++ js/controllers/pin.js
@@ -7,11 +7,11 @@
 
   PinController.prototype.activate = function () {
     this._pin.setOnClickPin(this._onClickPin.bind(this));
-    this._pin.getCard().setOnCloseCard(this._onCloseCard.bind(this));
   };
 
   PinController.prototype._onClickPin = function () {
     this._mainMap.renderCard(this._pin.getCard());
+    this._pin.getCard().setOnCloseCard(this._onCloseCard.bind(this));
   };
 
   PinController.prototype._onCloseCard = function () {
