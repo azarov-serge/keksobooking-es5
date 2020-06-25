515f0f1fd53c62a60860b230fe27fff8c0118a6e
ninzzya <s.a.azarov@gmail.com>
2020-06-25 13:00:11 +0300

    Add ID to order model. COde refactoring. Add filter for render pins

--- js/models/orders.js
+++ js/models/orders.js
@@ -1,19 +1,27 @@
 'use strict';
 (function () {
+  var Util = window.Util;
+
   function OrdersModel() {
     this._orders = null;
   }
 
   OrdersModel.prototype.setOrders = function (orders) {
-    this._orders = orders;
+    this._orders = orders.map(this._createOrder);
   };
 
   OrdersModel.prototype.getOrders = function () {
     return this._orders;
   };
 
-  OrdersModel.prototype.getOrderByIndex = function (index) {
-    return this._orders[index];
+  OrdersModel.prototype.getOrderByID = function (id) {
+    return Util.getByID(this._orders, parseInt(id, 10));
+  };
+
+  OrdersModel.prototype._createOrder = function (order, id) {
+    var orderModel = Object.assign({}, order);
+    orderModel.id = id;
+    return orderModel;
   };
 
   window.OrdersModel = OrdersModel;
