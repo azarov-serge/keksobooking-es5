d7173970d98a24dad491c9ac849d04db2554c0c6
Azarov Serge <s.a.azarov@gmail.com>
2021-08-12 20:44:16 +0300

    map-part-1: Update index.js

--- js/index.js
+++ js/index.js
@@ -4,15 +4,32 @@
   var AppView = window.AppView;
   var PromoView = window.PromoView;
 
+  // Import presenters
+  var MapPresenter = window.MapPresenter;
+
+  // Import models
+  var AppModel = window.AppModel;
+
   // Import utils
   var render = window.DomUtils.render;
   var RenderPosition = window.DomUtils.RenderPosition;
+
+  var appModel = new AppModel();
 
   var rootElement = document.getElementById('root');
   var appView = new AppView();
   var promoView = new PromoView();
 
+  var mapPresenter = new MapPresenter({
+    appView: appView,
+    promoView: promoView,
+    appModel: appModel,
+  });
+
   render(rootElement, appView, RenderPosition.BEFORE_END);
-  render(appView, promoView, RenderPosition.BEFORE_END);
+
+  mapPresenter.init();
+
+  render(appView, promoView, RenderPosition.AFTER_BEGIN);
 
 })();
