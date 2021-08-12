6abfc1f5caceda702e701d5bb92e77b583358c6e
Azarov Serge <s.a.azarov@gmail.com>
2021-08-12 20:43:57 +0300

    map-part-1: Add UpdateType constant

---
+++ js/constants/update-type.js
@@ -0,0 +1,12 @@
+'use strict';
+(function () {
+  var UpdateType = {
+    PATCH: 'PATCH',
+    MINOR: 'MINOR',
+    MAJOR: 'MAJOR',
+    INIT: 'INIT',
+    ERROR: 'ERROR',
+  };
+
+  window.UpdateType = UpdateType;
+})();
