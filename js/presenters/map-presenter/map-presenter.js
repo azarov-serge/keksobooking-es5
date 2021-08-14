28d51c3ee35e4ee0e1a4041af69e78aab215d862
Azarov Serge <s.a.azarov@gmail.com>
2021-08-14 13:38:20 +0300

    map-part-3: Add getting orders from backend and render pins (add orders models, API and new methods)

--- js/presenters/map-presenter/map-presenter.js
+++ js/presenters/map-presenter/map-presenter.js
@@ -5,10 +5,13 @@
   var MapFiltersPresenter = window.MapFiltersPresenter;
   var MapPinsContainerView = window.MapPinsContainerView;
   var MainPinView = window.MainPinView;
+  var PinView = window.PinView;
 
   // Import utils
   var render = window.DomUtils.render;
+  var remove = window.DomUtils.remove;
   var RenderPosition = window.DomUtils.RenderPosition;
+  var Api = window.Api;
 
   var UpdateType = window.UpdateType;
   var AppState = window.AppState;
@@ -24,10 +27,15 @@
     this._mapView = new MapView();
     this._mapPinsContainerView = new MapPinsContainerView();
     this._mainPinView = new MainPinView();
+    this._pinViews = [];
 
     this._mapFiltersPresenter = new MapFiltersPresenter({mapView: this._mapView});
 
     this._appModel = args.appModel;
+    this._ordersModel = args.ordersModel;
+
+    this._setOrders = this._setOrders.bind(this);
+    this._setError = this._setError.bind(this);
 
     this._handleMainPinMouseDown = this._handleMainPinMouseDown.bind(this);
     this._modelEventHandler = this._modelEventHandler.bind(this);
@@ -35,6 +43,7 @@
 
   MapPresenter.prototype.init = function () {
     this._appModel.subscribe(this._modelEventHandler);
+    this._ordersModel.subscribe(this._modelEventHandler);
 
 
     this._mapView.setToggleClass(MAP_TOGGLE_CLASS);
@@ -57,6 +66,11 @@
     render(this._appView, this._mapView, RenderPosition.AFTER_BEGIN);
   };
 
+  MapPresenter.prototype.destroy = function () {
+    this._appModel.unsubscribe(this._modelEventHandler);
+    this._ordersModel.unsubscribe(this._modelEventHandler);
+  };
+
   MapPresenter.prototype._toggleMapState = function () {
     var isNeedToggle = (
       (this._mapView.isHasToggleState && this._appModel.getState() === AppState.ACTIVATED) ||
@@ -73,31 +87,58 @@
     console.log('Start coords: ', coords);
 
     if (this._appModel.getState() === AppState.DEACTIVATED) {
-      this._appModel.setState(UpdateType.PATCH, AppState.ACTIVATED);
+      this._appModel.setState(UpdateType.INIT, AppState.ACTIVATED);
+      Api.getOrders()
+        .then(this._setOrders)
+        .catch(this._setError);
     }
   };
 
-  MapPresenter.prototype._updatePins = function () {
+  MapPresenter.prototype._setOrders = function (result) {
+    this._ordersModel.setOrders(UpdateType.MINOR, result.response);
+  };
 
+  MapPresenter.prototype._setError = function (error) {
+    this._ordersModel.setError(UpdateType.ERROR, error);
+  };
+
+  MapPresenter.prototype._updatePins = function () {
+    this._removePins();
+    this._renderPins();
+  };
+
+  MapPresenter.prototype._renderPins = function () {
+    var orders = this._ordersModel.getOrders();
+
+    for (var index = 0; index < orders.length; index++) {
+      this._pinViews.push(new PinView(orders[index]));
+    }
+
+    render(this._mapPinsContainerView, this._pinViews, RenderPosition.AFTER_BEGIN);
+  };
+
+  MapPresenter.prototype._removePins = function () {
+    this._pinViews.forEach(function (pinView) {
+      remove(pinView);
+    });
   };
 
 
-  MapPresenter.prototype._modelEventHandler = function (actionType, payload) {
+  MapPresenter.prototype._modelEventHandler = function (actionType, _payload) {
     switch (actionType) {
-      case UpdateType.PATCH:
+      case UpdateType.INIT:
         this._toggleMapState();
         break;
       case UpdateType.MINOR:
-        this._updatePins(payload);
+        // Rerender points
+        this._updatePins();
         break;
       case UpdateType.MAJOR:
-        this._updatePins(payload);
-        break;
-      case UpdateType.INIT:
-        this._updatePins(payload);
+        // Rerender points and reset filters
+        this._updatePins();
         break;
       case UpdateType.ERROR:
-        this._updatePins(payload);
+        this._renderError();
         break;
     }
   };
