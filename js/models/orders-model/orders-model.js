b6e273640fef2acc25c344bd2822d6b303173aac
Azarov Serge <s.a.azarov@gmail.com>
2021-08-14 13:28:28 +0300

    map-part-3: Add OrdersModel

---
+++ js/models/orders-model/orders-model.js
@@ -0,0 +1,33 @@
+'use strict';
+(function () {
+  var Observer = window.Observer;
+
+  function OrdersModel() {
+    Observer.call(this);
+    this._orders = [];
+    this._error = '';
+  }
+
+  OrdersModel.prototype = Object.create(Observer.prototype);
+  OrdersModel.prototype.constructor = OrdersModel;
+
+  OrdersModel.prototype.setOrders = function (actionType, orders) {
+    this._orders = orders;
+    this.notify(actionType);
+  };
+
+  OrdersModel.prototype.getOrders = function () {
+    return this._orders;
+  };
+
+  OrdersModel.prototype.setError = function (actionType, error) {
+    this._error = error;
+    this.notify(actionType);
+  };
+
+  OrdersModel.prototype.getError = function () {
+    return this._error;
+  };
+
+  window.OrdersModel = OrdersModel;
+})();
