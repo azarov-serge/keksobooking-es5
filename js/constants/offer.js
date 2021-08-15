7ebaf4f4246d6ba016f138b49e86aae961ffe145
Azarov Serge <s.a.azarov@gmail.com>
2021-08-15 08:14:07 +0300

    map-part-6: Add constants (offer)

---
+++ js/constants/offer.js
@@ -0,0 +1,16 @@
+'use strict';
+(function () {
+
+  window.offer = {
+    DEFAULT_AVATAR: 'img/muffin-grey.svg',
+    bookingType: {
+      any: 'Любой тип жилья',
+      palace: 'Дворец',
+      flat: 'Квартира',
+      house: 'Дом',
+      bungalo: 'Бунгало',
+    },
+  };
+})();
+
+
